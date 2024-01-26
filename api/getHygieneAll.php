7c7c8a4148aa84b9031d366a5acb65a49f4af50c
Henry Castillo <henrycastillome@gmail.com>
2024-01-25 21:55:17 -0500

    changes

---
+++ api/getHygieneAll.php
@@ -0,0 +1,59 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: *");
+
+require_once 'login.php';
+
+function sanitizeString($var)
+{
+    $var = stripslashes($var);
+    $var = strip_tags($var);
+    $var = htmlentities($var);
+    return $var;
+}
+
+try {
+    $pdo = new PDO($attr, $user, $pass, $opts);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Error connecting to the database: " . $e->getMessage();
+    exit;
+}
+
+
+$method = $_SERVER['REQUEST_METHOD'];
+switch ($method) {
+   case "GET":
+       
+       try{
+        $stmt = $pdo->prepare("SELECT * from items where item_category = 'Hygiene'");
+        $stmt->execute();
+        $allProducts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        if(count($allProducts)>0){
+            http_response_code(200); // ok
+        
+            echo json_encode(["data"=>["products"=>$allProducts]]);
+        }else{
+            http_response_code(404); //not found
+            echo json_encode(["error"=>"No products found."]);
+        }
+       }catch(PDOException $e){
+        http_response_code(500);//internal server error
+        echo json_encode(["error"=>"Error executing the query:  ".$e->getMessage()]);
+        exit;
+       }
+       break;
+       
+      
+    }
+
+    
+
+   
+        
+
+
+?>
--- food-pantry-tsc/src/pages/ProductPage.tsx
+++ food-pantry-tsc/src/pages/ProductPage.tsx
@@ -1,7 +1,46 @@
 import Header from "../sections/Header";
-import { Box, Heading } from "@chakra-ui/react";
+import React, { useState, useEffect } from "react";
+import { Box, Heading, HStack,Button,ButtonGroup, Grid, GridItem } from "@chakra-ui/react";
+import FullScreenSection from "../components/FullScreenSection";
+import { Item } from "../types/ItemTypes";
+import { useCartContext } from "../context/CartContext";
+import ProductsApi from "../api/ProductsApi";
+import Cards from "../components/Cards";
 
-const ProductPage = () => {
+const ProductPage:React.FC<{}> = () => {
+
+    const [productsPage, setProductsPage] = useState<Item[]>([]);
+    const { handleAddToCartClick } = useCartContext();
+    const [isActive, setIsActive]=React.useState<boolean>(false)
+
+    const fetchAllProducts = async () => {
+        
+        try {
+            const fetchProducts = new ProductsApi("http://localhost/my_php/food-pantry-ecommerce/api/getAllProducts.php");
+            const response = await fetchProducts.getAll();
+            setProductsPage(response.products);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    const fetchHygieneProducts = async () => {
+        setIsActive(false)
+        try {
+            const fetchProducts = new ProductsApi("http://localhost/my_php/food-pantry-ecommerce/api/getHygieneAll.php");
+            const response = await fetchProducts.getAll();
+            setProductsPage(response.products);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    const activeButton = () => {
+        
+    }
+    console.log(productsPage)
+
+   
   return (
     <main>
       <>
@@ -25,6 +64,70 @@ const ProductPage = () => {
             </Box>
           </Box>
         </Box>
+
+        <FullScreenSection
+        pt={16}
+        pb={16}
+        width="100vw"
+        backgroundColor="var(--color-white)"
+      >
+        <Box pb={8}>
+          <HStack spacing={4}>
+            <Heading fontSize="lg"> Filter by: </Heading>
+            <ButtonGroup variant="outline" colorScheme="teal" spacing={4}>
+              <Button isActive={isActive}  onClick={()=>fetchAllProducts()} >
+                All Products
+              </Button>
+              <Button onClick={()=>fetchHygieneProducts()}>
+                {" "}
+                Hygiene
+              </Button>
+              <Button >
+                {" "}
+                Food
+              </Button>
+              <Button >
+                {" "}
+                Household Items
+              </Button>
+            </ButtonGroup>
+          </HStack>
+        </Box>
+
+        <Box
+          width="80vw"
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          gap={16}>
+
+            {productsPage.length===0 ?(
+                <Box>Loading...</Box>
+            ) :(
+                <Grid templateColumns="repeat(3,1fr)" gap={6}>
+                    {productsPage.map((product,index)=>{
+                        return(
+                            <GridItem key={index}>
+                                <Cards
+                                    key={product.item_id}
+                                    src={decodeURIComponent(product.item_image)}
+                                    product={product.item_name}
+                                    category={product.item_category}
+                                    quantity={product.item_quantity}
+                                    button2="Add to Cart"
+                                    onClick={()=>handleAddToCartClick(product)}
+                                    />
+                            </GridItem>
+                        )
+                    })}
+                    </Grid>
+                
+            )}
+          </Box>
+
+
+
+      </FullScreenSection>
       </>
     </main>
   );
